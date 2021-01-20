142d198fd2226675826e8900e4ede053aba43704
shiincs <skywing27@gmail.com>
2021-01-20 17:03:52 +0900

    config: eslint/babel 설정 변경
    - babel config file extension 변경 (json -> js)
    - babel plugin 제거 (optional-chaining, nullish-coalescing)
    - @babel/preset-env option 추가 (polyfill added by core-js@3.8)
    - eslintrc plugins에 있던 것들 extends로 합침

---
+++ .babelrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: {
+          version: '3.8',
+          proposals: false,
+        },
+        targets: {
+          ie: 11,
+        },
+      },
+    ],
+    ['@babel/preset-react'],
+  ],
+};
