65c7612396d1b8739cc7f65aac42ab29255d547c
sbyeol3 <ahnsbyeol3@gmail.com>
2021-01-28 12:36:52 +0900

    feat :  폰트 추가 (#3)

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -3,6 +3,22 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    @font-face {
+        font-family: 'UhBeeSeulvely';
+        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_five@.2.0/UhBeeSeulvely.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: "UhBeeSeulvely", -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    }
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
 `;
 
 export default GlobalStyle;
