2178b7f3a80b12ac2de0b56c0fe10a325ba3974e
sbyeol3 <ahnsbyeol3@gmail.com>
2021-01-28 12:24:34 +0900

    feat : styled reset 설정 (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/pages/App';
+import GlobalStyle from '@/styles/Global';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+);
