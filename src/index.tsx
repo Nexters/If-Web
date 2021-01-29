133c9fbe38840bf36669f4e4060c58fdca46b1eb
sbyeol3 <ahnsbyeol3@gmail.com>
2021-01-29 11:38:14 +0900

    chore : global 속성 App으로 이동

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/pages/App';
-import GlobalStyle from '@/styles/global';
-import theme from '@/styles/theme';
-import { ThemeProvider } from 'styled-components';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
