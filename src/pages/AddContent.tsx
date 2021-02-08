799e903952afbdffb8df4a976777e03bacd86b2a
sbyeol3 <ahnsbyeol3@gmail.com>
2021-02-08 23:35:41 +0900

    feat : addcontent 페이지 수정 및 theme 수정 (#21)

--- src/pages/AddContent.tsx
+++ src/pages/AddContent.tsx
@@ -1,7 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { atom, useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+import TitleInput from '@/components/TitleInput';
+import LocationText from '@/components/LocationText';
+import NationText from '@/components/NationText';
+import ContentInput from '@/components/ContentInput';
+import ImageList from '@/components/ImageList';
+
+const AddContentWrapper = styled.div`
+  padding: 0 24px;
+`;
+
+const imageList = atom({
+  key: 'imageList',
+  default: [],
+});
 
 const AddContent: FC = () => {
-  return <div>콘텐츠 추가 페이지</div>;
+  const [location, setLocation] = useState('음식을 먹은 장소');
+  const [nation, setNation] = useState(null);
+  const imageListData = useRecoilValue(imageList);
+  return (
+    <AddContentWrapper>
+      <TitleInput />
+      <LocationText location={location} />
+      <NationText nation={nation} />
+      <ImageList imageList={imageListData} />
+      <ContentInput />
+    </AddContentWrapper>
+  );
 };
 
 export default AddContent;
