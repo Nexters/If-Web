8526f0c41cea023c3379c7c880e80a92231e6791
sbyeol3 <ahnsbyeol3@gmail.com>
2021-02-14 02:41:19 +0900

    feat : title, content recoil value

--- src/components/ContentInput.tsx
+++ src/components/ContentInput.tsx
@@ -1,7 +1,36 @@
 import React, { FC } from 'react';
+import { IStoryState } from '@/types';
+import { RecoilState, useRecoilState } from 'recoil';
 import styled from 'styled-components';
 
-const Input = styled.textarea`
+interface IContentInputProps {
+  storyAtom: RecoilState<IStoryState>;
+}
+
+const ContentInput: FC<IContentInputProps> = ({ storyAtom }) => {
+  const [storyState, setStoryState] = useRecoilState(storyAtom);
+
+  const onChangeContent = (value: string) =>
+    setStoryState({
+      ...storyState,
+      memo: value,
+    });
+
+  return (
+    <>
+      <Line />
+      <TextArea
+        value={storyState.memo}
+        onChange={({ target }) => onChangeContent(target.value)}
+        placeholder="무엇을 드셨나요?"
+      ></TextArea>
+    </>
+  );
+};
+
+export default ContentInput;
+
+const TextArea = styled.textarea`
   width: 100%;
   min-height: 400px;
   padding: 0;
@@ -22,14 +51,3 @@ const Line = styled.hr`
   margin: 24px 0;
   color: ${(props) => props.theme.colors.lightgray};
 `;
-
-const ContentInput: FC = () => {
-  return (
-    <>
-      <Line />
-      <Input placeholder="무엇을 드셨나요?"></Input>
-    </>
-  );
-};
-
-export default ContentInput;
--- src/components/TitleInput.tsx
+++ src/components/TitleInput.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import { RecoilState, useRecoilState } from 'recoil';
+import { IStoryState } from '@/types';
+
+interface ITitleInputProps {
+  storyAtom: RecoilState<IStoryState>;
+}
 
 const Input = styled.input`
   width: 100%;
@@ -14,8 +20,19 @@ const Input = styled.input`
   }
 `;
 
-const TitleInput: FC = () => {
-  return <Input placeholder="제목을 입력해주세요 :)"></Input>;
+const TitleInput: FC<ITitleInputProps> = ({ storyAtom }) => {
+  const [storyState, setStoryState] = useRecoilState(storyAtom);
+
+  const onChangeTitle = (value: string) =>
+    setStoryState({ ...storyState, title: value });
+
+  return (
+    <Input
+      placeholder="제목을 입력해주세요 :)"
+      onChange={(e) => onChangeTitle(e.target.value)}
+      value={storyState.title}
+    ></Input>
+  );
 };
 
 export default TitleInput;
