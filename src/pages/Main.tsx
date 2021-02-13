b2ddb4117b984ba516ed0e27cd5ee8c0076e59e7
sbyeol3 <ahnsbyeol3@gmail.com>
2021-02-14 00:44:51 +0900

    feat : Main API 연결

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,33 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Story from '@/components/Story';
 import AddButton from '@/components/AddButton';
+import HEADER_TYPES from '@/types/HeaderTypes';
+import request from '@/utils/request';
+import Header from './Header';
 
 const Main: FC = () => {
+  const [storyList, setStoryList] = useState([]);
+
+  async function getStoryList() {
+    const data = await request({
+      url: '/stories',
+      method: 'GET',
+    });
+    setStoryList(data);
+  }
+
+  useEffect(() => {
+    getStoryList();
+  }, []);
+
   return (
     <>
-      <Story position="left" date="2021-01-30" memo="test" />
+      <Header type={HEADER_TYPES.FEED} />
+      {storyList.map((story, idx) => {
+        const { id } = story;
+        const pos = idx % 2 === 0 ? 'left' : 'right';
+        return <Story key={id} position={pos} {...story} />;
+      })}
       <AddButton />
     </>
   );
