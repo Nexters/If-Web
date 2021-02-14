8c4928fdaa57de42088daf5f66bc51380a5cdeeb
sbyeol3 <ahnsbyeol3@gmail.com>
2021-02-14 15:02:02 +0900

    feat : location storystate 연결 (#21)

--- src/components/LocationText.tsx
+++ src/components/LocationText.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import { useStoryState } from '@/atoms/storyState';
 import Icon from './FeatureIcon';
 
 const LocationInput: FC = () => {
+  const { storyState, setStoryState } = useStoryState();
+
   return (
     <Wrapper>
       <Icon name="location" />
-      <LocationText>
-        {/* {storyState.experience_place || '음식을 먹은 장소'} */}
-      </LocationText>
+      <LocationText>{storyState.place || '음식을 먹은 장소'}</LocationText>
       <Text>에서 느낀</Text>
     </Wrapper>
   );
