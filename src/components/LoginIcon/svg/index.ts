faf16218bc72cd878130aeac88d2a099e948340f
danakim21 <danakim21@gmail.com>
2021-02-17 00:29:25 +0900

    improvement: 일러스트 추가, 네이버 로그인 버튼 변경

--- src/components/LoginIcon/svg/index.ts
+++ src/components/LoginIcon/svg/index.ts
@@ -1,4 +1,6 @@
 import { ReactComponent as kakao } from '@/components/LoginIcon/svg/kakao.svg';
 import { ReactComponent as naver } from '@/components/LoginIcon/svg/naver.svg';
+import { ReactComponent as naver2 } from '@/components/LoginIcon/svg/naver2.svg';
+import { ReactComponent as login } from '@/components/LoginIcon/svg/login.svg';
 
-export { kakao, naver };
+export { kakao, naver, naver2, login };
