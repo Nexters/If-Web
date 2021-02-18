10f0d1e33976682bcc1818d773c0f056d75ce6e0
sbyeol3 <ahnsbyeol3@gmail.com>
2021-02-19 01:09:57 +0900

    feat : request data 추가

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,12 +3,14 @@ import axios from 'axios';
 interface IRequestProps {
   url: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  data?: any;
 }
 
 export default async (props: IRequestProps) => {
   const { data } = await axios({
     url: `/api${props.url}`,
     method: props.method,
+    data: props.data,
     headers: {
       Authorization: `Bearer ${process.env.TEST_TOKEN}`,
     },
